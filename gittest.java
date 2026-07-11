de4c6078e2728b5490a9ef6790af26e149e971fb
Abhishek Kumar <6602028@runta.org>
2026-07-11 19:55:16 +0530

    notification

--- gittest.java
+++ gittest.java
@@ -1,5 +1,14 @@
 public class gittest {
     public static void main(String[] args) {
         System.out.println("I'm Generating a new file for testing git");
+        System.out.println("This is a new line added to test git commit");
+    }
+    
+    public void Payment() {
+        System.out.println("Payment method is called");
+    }
+
+    public void notification() {
+        System.out.println("Notification method is called");
     }
 }
